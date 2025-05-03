217da95e11f01b8d22c13f8fe1e00c3dc08be8d2
Kiryha <chapatisi93@gmail.com>
2025-05-03 03:06:42 +0300

    update: comment sheet

--- app/src/main/java/com/example/diplomwork/presentation/ui/components/CommentSheet.kt
+++ app/src/main/java/com/example/diplomwork/presentation/ui/components/CommentSheet.kt
@@ -5,11 +5,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -64,7 +68,7 @@ fun CommentsBottomSheet(
         object : NestedScrollConnection {
             override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
                 return if (available.y > 0 && sheetState.currentValue != SheetValue.Hidden) {
-                    Offset.Zero // перехватываем вверхний свайп
+                    Offset.Zero
                 } else {
                     Offset.Zero
                 }
@@ -72,8 +76,11 @@ fun CommentsBottomSheet(
         }
     }
 
+    val statusBarHeight = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()
+
     ModalBottomSheet(
         onDismissRequest = onDismiss,
+        modifier = Modifier.padding(top = statusBarHeight),
         sheetState = sheetState,
         shape = RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp),
         containerColor = BgDefault,
@@ -106,8 +113,7 @@ fun CommentsContent(
 
     Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(0.92f)
+            .fillMaxSize()
     ) {
         Row(
             modifier = Modifier
